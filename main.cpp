cb14372b45c23dfa179d69201dbce50f6d8bd1f2
JIN07250725 <jys0725@outlook.com>
2025-06-28 20:07:29 +0000

    Design in progress

--- main.cpp
+++ main.cpp
@@ -48,6 +48,25 @@ int main() {
 
 // ------------- DESIGN -------------
 /* 
+
+Read the employee ID number (an int) from the user
+
+Read the number of hours worked (an int) from the user
+
+Read the hourly rate (an int) from the user
+
+Read the federal withholding rate (an int, e.g., 10)
+
+You must read one input per line. Please see the sample run below. This is part of the required coding construct.
+
+Your program should calculate the total gross pay, the Income Tax Withholding, and the net pay.
+
+First calculate the total gross pay from the hours worked and the hourly rate inputs.
+
+Then calculate the Federal Tax withholding using the gross pay and the federal withholding rate.
+
+Then calculate the Net Pay by subtracting the federal tax withholding from the gross pay.
+
 Program Name:
 
 Program Description:
@@ -55,6 +74,7 @@ Program Description:
 Design:
 A. INPUT
 Define the input variables including name data type. 
+int employee_id = 0;
 
 B. OUTPUT
 Define the output variables including data types. 
@@ -70,8 +90,33 @@ Use conditionals, loops, functions or array constructs.
 List the steps in transforming inputs into outputs. 
 https://github.com/Glen-Sasek-PCC-Instructor/2025-06-22/blob/main/Pseudocode-Reference.txt
 
+PROMPT "Enter your employee ID number"
+INPUT employee_id
 
 SAMPLE RUNS
 Copy from assignment document.
+Welcome to my Weekly Payroll program!!
+Enter your employee ID number (numbers only): 34567
+Enter number of hours worked (whole numbers): 40
+Enter the hourly rate: 17
+Enter the federal withholding rate: 15
 
+Your Payroll Summary:
+Total Gross Pay: $680
+Federal Tax Withholding: $102
+Net Pay: $578
+
+Thank you for using my Weekly Payroll program!!
+Welcome to my Weekly Payroll program!!
+Enter your employee ID number (numbers only): 12345
+Enter number of hours worked (whole numbers): 30
+Enter the hourly rate: 22
+Enter the federal withholding rate: 10
+
+Your Payroll Summary:
+Total Gross Pay: $660
+Federal Tax Withholding: $66
+Net Pay: $594
+
+Thank you for using my Weekly Payroll program!!
 */
